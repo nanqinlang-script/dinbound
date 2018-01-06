18c9c5133b3525415925dcd92b6274f5fb8fc177
南琴浪 <contacts@sometimesnaive.org>
2018-01-06 06:59:11 -0600

    bash

--- dinbound.sh
+++ dinbound.sh
@@ -3,36 +3,63 @@
 # download dir
 directory(){
 	[[ ! -d /home/dinbound ]] && mkdir -p /home/dinbound
+}
+
+dingo-download(){
 	cd /home/dinbound
-}
-
-download(){
 	wget https://raw.githubusercontent.com/nanqinlang/dinbound/master/dingo-linux-amd64
-	wget https://raw.githubusercontent.com/nanqinlang/dinbound/master/unbound1.tar.gz && tar -zxf unbound1.tar.gz
-	wget https://raw.githubusercontent.com/nanqinlang/dinbound/master/unbound2.tar.gz && tar -zxf unbound2.tar.gz
-}
-
-# port 53 + 55
-unbound(){
-	cp /home/dinbound/unbound1 /home/unbound1 && chmod -R 7777 /home/unbound1 && /home/unbound1/sbin/unbound
-	cp /home/dinbound/unbound2 /home/unbound2 && chmod -R 7777 /home/unbound2 && /home/unbound2/sbin/unbound
 }
 
 # port 54
-dingo(){
+dingo-start(){
 	mkdir /home/dingo
-	cp /home/dinbound/dingo-linux-amd64 /home/dingo/dingo-linux-amd64
-	chmod -R 7777 /home/dingo
-	cd /home/dingo && nohup ./dingo-linux-amd64 -port=54 -bind=127.0.0.1 -gdns:auto &
+	cd /home/dinbound
+	mv dingo-linux-amd64 /home/dingo/dingo-linux-amd64
+	cd /home/dingo
+	chmod +x dingo-linux-amd64
+	nohup ./dingo-linux-amd64 -port=54 -bind=127.0.0.1 -gdns:auto &
 }
 
 
-# main:
+unbound-compile(){
+	cd /home/dinbound
+	wget https://raw.githubusercontent.com/nanqinlang-script/unbound/master/unbound-1.6.7.tar.gz && tar -zxf unbound-1.6.7.tar.gz
+	cd unbound-1.6.7
+	apt-get update && apt-get install -y build-essential && apt-get build-dep -y unbound
+
+	./configure --prefix=/home/unbound1 --sysconfdir=/home/unbound1/etc --with-pidfile=/home/unbound1/pid/unbound.pid --with-libevent --disable-rpath
+	make && make install
+
+	make clean
+
+	./configure --prefix=/home/unbound2 --sysconfdir=/home/unbound2/etc --with-pidfile=/home/unbound2/pid/unbound.pid --with-libevent --disable-rpath
+	make && make install
+}
+
+unbound-config(){
+	cd /home/dinbound
+	wget https://raw.githubusercontent.com/nanqinlang-script/unbound/master/unbound1.conf
+	wget https://raw.githubusercontent.com/nanqinlang-script/unbound/master/unbound2.conf
+	mv -f unbound1.conf /home/unbound1/etc/unbound/unbound.conf
+	mv -f unbound2.conf /home/unbound2/etc/unbound/unbound.conf
+}
+
+unbound-start(){
+	# port 53
+	chmod -R 0777 /home/unbound1 && /home/unbound1/sbin/unbound
+	# port 55
+	chmod -R 0777 /home/unbound2 && /home/unbound2/sbin/unbound
+}
+
+
+
 directory
-download
-unbound
-dingo
-#rm -rf /home/dinbound
+dingo-download
+dingo-start
+unbound-compile
+unbound-config
+unbound-start
+rm -rf /home/dinbound
 
 # change resolv.conf
 mv /etc/resolv.conf /etc/resolv.conf.bak
